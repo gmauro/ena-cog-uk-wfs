1f5155217508cd90c2e2b377225e7b2536081ec5
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-03-26 16:11:38 +0100

    Update run_reporting.sh

--- run_reporting.sh
+++ run_reporting.sh
@@ -13,13 +13,16 @@ JOB_YML_DIR='job-yml-templates'
 
 # start processing
 WORKDIR=$BOT_TAG'_run_'$(date '+%s')
-mkdir $WORKDIR &&
-echo "$JOB_YML_DIR/$JOB_YML"
+mkdir $WORKDIR
+date
+echo "Generating yaml file $JOB_YML_DIR/$JOB_YML"
 #generate the job.yml needed by planemo run
 cat "$JOB_YML_DIR/$JOB_YML" | python bioblend-scripts/find_datasets.py "$VCF_DATA" -g "https://usegalaxy.eu" -a $API_KEY -t $BOT_TAG --collections-only -n 1 --from-template -o "$WORKDIR/$JOB_YML"
 
 # TO DO: remove $BOT_TAG from history - as part of find_datasets.py or via separate script
 if [ -s "$WORKDIR/$JOB_YML" ]; then
+    date
+    echo "Starting Planemo"
     # if the formatted job.yml file contains data, we know a suitable source history was found
     # otherwise we assume no history is ready for processing
     SOURCE_HISTORY_ID=$(grep '#from_history_id:' "$WORKDIR/$JOB_YML" | cut -d ' ' -f 2-)
@@ -29,6 +32,7 @@ if [ -s "$WORKDIR/$JOB_YML" ]; then
     # run the viral beacon WF
     planemo -v run 4e9e995d3ce690bf "$WORKDIR/$JOB_YML" --history_name "$SOURCE_HISTORY_NAME - $DEST_NAME_SUFFIX" --galaxy_url 'https://usegalaxy.eu' --galaxy_user_key $API_KEY --engine external_galaxy 2>&1 > /dev/null | grep -o 'GET /api/histories/[^?]*\?' > "$WORKDIR/run_info.txt"
 fi
-
+date
+echo "Cleaning"
 rm -R $WORKDIR
 
